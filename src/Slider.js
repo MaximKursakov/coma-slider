161a3e88ae52b419ff5ea3222c10a015c51f9b46
maxim <maximcarlos@gmx.de>
2022-11-15 14:18:22 +0100

    position detected

--- src/Slider.js
+++ src/Slider.js
@@ -1,3 +1,6 @@
+import { motion } from "framer-motion"
+import { useState } from "react"
+
 export function Slider() {
     const mitarbeiterCollection = [{
         image: "Mitarbeiter1", name: "Riccardo", title: "UX Designer & Perfektionist", location: "Coma AG (München)" , description: "Riccardo ist stets auf der Suche nach der perfekten Lösung und gibt sich nicht mit dem Zweitbesten zufrieden. Mit Crossfit hält er sich auch körperlich in optimaler Form. Als ausgesprochener Familienmensch verbringt er seine Freizeit aber am liebsten mit Frau und Tochter beim Grillen im Garten oder Seriengucken im eigenen Heimkino.", id: 1
@@ -14,22 +17,37 @@ export function Slider() {
     {
         image: "Mitarbeiter1", name: "Riccardo", title: "UX Designer & Perfektionist", location: "Coma AG (München)" , description: "Riccardo ist stets auf der Suche nach der perfekten Lösung und gibt sich nicht mit dem Zweitbesten zufrieden. Mit Crossfit hält er sich auch körperlich in optimaler Form. Als ausgesprochener Familienmensch verbringt er seine Freizeit aber am liebsten mit Frau und Tochter beim Grillen im Garten oder Seriengucken im eigenen Heimkino.", id: 5
     }]
+
+    const [position, setPosition] = useState(1)
+
+    function slideRight() {
+        if (position < mitarbeiterCollection.length ) {
+            setPosition(position + 1)
+        }
+    }
+
+    function slideLeft() {
+        if (position-1  > 0) {
+            setPosition(position - 1)
+        }
+    }
+    console.log(position)
     return(
         <div className="slider-container">
-            <button className="slide-left"> left </button>
-            <button className="slide-right"> right </button>
+            <button className="slide-left" onClick={slideLeft}> left </button>
+            <button className="slide-right" onClick={slideRight}> right </button>
             <div className="slider">
                 {mitarbeiterCollection.map((mitarbeiter) => {
                     return (
-                        <div key={mitarbeiter.id} className="slider-item">
-                            <div className="slider-image" style={{backgroundImage: "url(/images/Mitarbeiter1.png)",}}></div>
+                        <motion.div key={mitarbeiter.id} className="slider-item">
+                            <div className="slider-image" style={{backgroundImage: `url(/images/${mitarbeiter.image}.png)`,}}></div>
                             <div className="slider-info">
                                 <h2>{mitarbeiter.name}</h2>
                                 <h3>{mitarbeiter.title}</h3>
                                 <p>{mitarbeiter.description}</p>
                                 <p>{mitarbeiter.location}</p>
                             </div>
-                        </div>
+                        </motion.div>
                     )
                 })}
             </div>
