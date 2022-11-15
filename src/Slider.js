1863ca2bfebb076fa87eed7b393a65ea49dcfd02
maxim <maximcarlos@gmx.de>
2022-11-15 13:35:21 +0100

    images converted and imported + basic array ob objects with slider data

---
+++ src/Slider.js
@@ -0,0 +1,10 @@
+export function Slider() {
+    const mitarbeiter = [{
+        image: "Mitarbeiter1", name: "Riccardo", title: "UX Designer & Perfektionist", location: "Coma AG (München)" , description: "Riccardo ist stets auf der Suche nach der perfekten Lösung und gibt sich nicht mit dem Zweitbesten zufrieden. Mit Crossfit hält er sich auch körperlich in optimaler Form. Als ausgesprochener Familienmensch verbringt er seine Freizeit aber am liebsten mit Frau und Tochter beim Grillen im Garten oder Seriengucken im eigenen Heimkino."
+    }]
+    return(
+        <>
+
+        </>
+    )
+}
