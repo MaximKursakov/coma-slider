4365e932e7dab8b3864a352f5ce493d2bbfc4ab6
Maxim <maximcarlos@gmx.de>
2022-11-17 10:40:08 +0100

    typo

--- src/Slider.js
+++ src/Slider.js
@@ -70,7 +70,7 @@ export function Slider() {
         image: "Mitarbeiter22", name: "Eileen", title: "Digital Marketing Managerin & Grenzgängerin", location: "Coma AG (München)" , description: "Riccardo ist stets auf der Suche nach der perfekten Lösung und gibt sich nicht mit dem Zweitbesten zufrieden. Mit Crossfit hält er sich auch körperlich in optimaler Form. Als ausgesprochener Familienmensch verbringt er seine Freizeit aber am liebsten mit Frau und Tochter beim Grillen im Garten oder Seriengucken im eigenen Heimkino.", id: 22
     },
     {
-        image: "Mitarbeiter23", name: "Elon Musk", title: "Putzkraft & Geringverdiener", location: "Coma AG (München)" , description: "putzt manchaml ok", id: 23
+        image: "Mitarbeiter23", name: "Elon Musk", title: "Putzkraft & Geringverdiener", location: "Coma AG (München)" , description: "putzt manchmal ok", id: 23
     }]
 
     const [position, setPosition] = useState(1)
